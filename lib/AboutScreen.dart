eee1e643cf8e8b9181c96942753458140c3aebca
MH-Pollob <cochipollob@gmail.com>
2023-01-05 23:12:14 +0600

    2 nd update

--- lib/AboutScreen.dart
+++ lib/AboutScreen.dart
@@ -1,17 +1,61 @@
+import 'dart:ui';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
-class AboutScreen extends StatelessWidget {
+class AboutScreen extends StatefulWidget {
   const AboutScreen({super.key});
 
+  @override
+  State<AboutScreen> createState() => _AboutScreenState();
+}
+
+class _AboutScreenState extends State<AboutScreen> {
   @override
   Widget build(BuildContext context) {
     final args = ModalRoute.of(context)!.settings.arguments as Map;
     final name = args['name'];
     final phone = args['phone'];
     final email = args['email'];
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.amberAccent,
+        title: Text('$name'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Container(
+          width: double.infinity,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Center(
+                child: CircleAvatar(
+                  backgroundImage: AssetImage('assets/dp2.PNG'),
+                  radius: 40.0,
+                ),
+              ),
+              Divider(
+                height: 80.0,
+                color: Colors.grey[800],
+              ),
+              Text(
+                'Phone: $phone',
+                style: TextStyle(fontSize: 20.0),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Text(
+                'Email: $email',
+                style: TextStyle(fontSize: 20.0),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -2,18 +2,24 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
   final List<Map> users = [
     {"name": "pollob", "phone": 01745334044, "email": 'pollob@gmail.com'},
     {"name": "moynull", "phone": 0172347588, "email": 'moynull@gmail.com'},
   ];
 
-  HomeScreen({super.key});
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
+          backgroundColor: Colors.amberAccent,
           title: Text('Contacts'),
         ),
         body: ListView.builder(
